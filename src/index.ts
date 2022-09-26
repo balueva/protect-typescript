468ddfbe7e1808d1218f928658d083e1958fa1e8
balueva <81beu@mail.ru>
2022-09-26 10:19:25 +0700

    lesson 01

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { renderBlock } from './lib.js'
+import { renderSearchFormBlock } from './search-form.js'
+import { renderSearchStubBlock } from './search-results.js'
+import { renderUserBlock } from './user.js'
+//import { renderToast } from './lib.js'
+
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('Alisa Warren', 'avatar.png', 0)
+  renderSearchFormBlock()
+  //renderSearchStubBlock()
+  //renderToast(
+  //  { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
+  //  { name: 'Понял?', handler: () => { console.log('Уведомление закрыто') } }
+  //)
+})
---
+++ src/user.ts
@@ -0,0 +1,23 @@
+import { renderBlock } from './lib.js'
+
+export function renderUserBlock(userName: string, avatar: string, favoriteItemsAmount: number) {
+  const favoritesCaption = favoriteItemsAmount > 0 ? favoriteItemsAmount : 'ничего нет'
+  const hasFavoriteItems = favoriteItemsAmount > 0;
+
+  renderBlock(
+    'user-block',
+    `
+    <div class="header-container">
+      <img class="avatar" src="/img/${avatar}" alt="${userName}" />
+      <div class="info">
+          <p class="name">${userName}</p>
+          <p class="fav">
+            <i class="heart-icon${hasFavoriteItems ? ' active' : ''}"></i>${favoritesCaption}
+          </p>
+      </div>
+    </div>
+    `
+  )
+}
+
+// Wade Warren
